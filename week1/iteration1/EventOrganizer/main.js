91efd6fedc4eb283f67399b78257e5fa755ffd63
Konstantin Gogov <konstantin.gogov@gmail.com>
2020-11-12 18:34:55 +0200

    Refactored the whole assignment, fixed many bugs

--- week1/iteration1/EventOrganizer/main.js
+++ week1/iteration1/EventOrganizer/main.js
@@ -1,26 +1,71 @@
-// Main storage for Parties
-const EVENT_STORAGE_COLLECTION = [];
+// Storage
+let PARTIES_COLLECTION = [];
 
-// Helpers
-const isValidString = function(str) {
+const listAllParties = () => {
+    if (PARTIES_COLLECTION.length > 0) {
+
+        PARTIES_COLLECTION.forEach((party) => {
+            console.log(
+                `Event name: ${party.partyName};` +
+                ` Is suitable for minors: %c${party.isSuitableForMinors}`,
+                'background-color: #fff; font-size: 1rem; color: #000;'
+            );
+        });
+    } else {
+        console.warn('There are no events!');
+    }
+}
+
+const checkIfIDExists = function(collection, id) {
+    return collection.find((item) => item.ID == id);
+}
+
+const deletePartyByID = (ID) => {
+    if (checkIfIDExists(PARTIES_COLLECTION, ID)) {
+
+        PARTIES_COLLECTION = PARTIES_COLLECTION
+            .filter(party => party.ID !== ID);
+        
+        console.log(`%cParty was successfully deleted!`, 'background-color: green');
+    } else {
+        console.warn('Event with such ID was not found!');
+    }
+}
+
+const updatePartyByID = (ID, updateName, isSuitableForMinors = false) => {
+    if (checkIfIDExists(PARTIES_COLLECTION, ID)) {
+
+        verifyPartyCreation(updateName, isSuitableForMinors);
+
+        let partyIndex = PARTIES_COLLECTION
+            .findIndex(party => party.ID == ID);
+
+        console.log('Before update: ', PARTIES_COLLECTION[partyIndex]);
+
+        PARTIES_COLLECTION[partyIndex].partyName = updateName;
+        PARTIES_COLLECTION[partyIndex].isSuitableForMinors = isSuitableForMinors;
+
+        console.log('After update: ', PARTIES_COLLECTION[partyIndex]);
+
+    } else {
+        console.warn('Event with such ID was not found!');
+    }
+}
+
+
+// Validator helper functions
+const isNameValid = function(str) {
 
     return str !== null     && 
     typeof str === "string" && 
     str.length > 0;
 }
 
-const isNameValid = function(name) {
-
-    return name !== null     && 
-    typeof name === "string" &&
-    name.length > 2;
-}
-
-const isValidBool = function(bool) {
+const isBoolValid = function(bool) {
     return typeof bool === 'boolean';
 }
 
-const isSexValid = function(sex) {
+const isGenderValid = function(sex) {
 
     if (sex && typeof sex === "string") {
         
@@ -35,32 +80,141 @@ const isSexValid = function(sex) {
     return false;
 }
 
-const isAgeValidInt = function(value) {
+const isInt = function(value) {
 
-    function isInt(value) {
-
-        if (isNaN(value)) {
-            return false;
-        }
-
-        let num = parseFloat(value);
-            return (num | 0) === num;
+    if (isNaN(value)) {
+        return false;
     }
+
+    let num = parseFloat(value);
+        return (num | 0) === num;
+}
+
+const isAgeValid = function(value) {
 
     const MIN_AGE_TO_PARTY = 16;
 
-    if (isInt(value) && value >= MIN_AGE_TO_PARTY) {
-        return true;
+    return (isInt(value) && value >= MIN_AGE_TO_PARTY) ? true : false;
+}
+
+const verifyPartyCreation = function(name, boolean) {
+
+    if (!(isNameValid(name) && isBoolValid(boolean))) {
+        throw new TypeError('Please enter correct fields!');
+    }
+
+    return true;
+}
+
+const generateEventID = function() {
+
+    const hex = function(value) {
+        return Math.floor(value).toString(16)
+    }
+
+    return hex(Date.now() / 1000) +
+      ' '.repeat(16).replace(/./g, () => hex(Math.random() * 16))
+}
+
+function Party(partyName, isSuitableForMinors = false) {
+
+    verifyPartyCreation(partyName, isSuitableForMinors);
+
+    let party = Object.create(Party.prototype);
+
+    party.ID                    = generateEventID()
+    party.partyName             = partyName;
+    party.isSuitableForMinors   = isSuitableForMinors;
+    party.isOpenForReservations = true;
+    party.clientsCollection     = [];
+
+    PARTIES_COLLECTION.push(party);
+
+    return party;
+}
+
+Party.prototype.toggleReservations = function() {
+    if (!this.isOpenForReservations) {
+        this.isOpenForReservations = true;
+
+        console.log(`%cReservations are turned on!`,
+            'background-color: green;');
     } else {
-        return false;
+        this.isOpenForReservations = false;
+
+        console.log(`%cReservations are turned off!`,
+            'background-color: #961209;');
     }
 }
 
-const capitalizeFirstLetter = function(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+Party.prototype.getFullEventInfo = function() {
+    console.table(this);
 }
 
-// Utilities
+Party.prototype.addClient = function(client) {
+    if (!(client instanceof Client)) {
+        throw new Error('Please add valid client object!');
+    }
+
+    const ADULT_AGE = 18;
+
+    const checkIfClientIsEligible = (client) => {
+
+        if (client.age < ADULT_AGE && this.isSuitableForMinors === false) {
+            console.warn('You cannot sign for this party! Please look for other events!');
+        } else {
+            this.clientsCollection.push(client);
+
+            console.log(`%cClient added successfully!`,
+                'background-color: green;')
+        }
+    }
+
+    checkIfClientIsEligible(client);
+}
+
+Party.prototype.listClients = function(gender = '') {
+
+    if (!isGenderValid(gender)) {
+
+        this.clientsCollection.forEach(client => {
+            console.log(`Client: ${client.fullName}, ` +
+                        `Age ${client.age}, `         +
+                        `Gender: ${client.gender}`);
+        })
+
+    } else {
+        let filteredArr = this.clientsCollection
+            .filter(client => client.gender === gender);
+
+        return filteredArr
+            .forEach(client => console.log(`${client.fullName}`));
+        
+    }
+}
+
+Party.prototype.removeClient = function(ID) {
+    if (checkIfIDExists(this.clientsCollection, ID)) {
+
+        this.clientsCollection = this.clientsCollection
+            .filter(client => client.ID !== ID);
+        
+        console.log(`%cClient was successfully deleted!`, 'background-color: green');
+    } else {
+        console.warn('Client with such ID was not found!');
+    }
+}
+
+const verifyClientCreation = function(firstName, lastName, gender, age) {
+
+    if( !(isNameValid(firstName) &&
+        isNameValid(lastName)    &&
+        isGenderValid(gender)    &&
+        isAgeValid(age))) throw new TypeError('Please enter correct values!');
+
+    return true;
+}
+
 const generateClientID = function() {
 
     const length = 8;
@@ -87,258 +241,38 @@ const generateClientID = function() {
     return generateID();
 }
 
-const generateEventID = function() {
-
-    const hex = function(value) {
-        return Math.floor(value).toString(16)
-    }
-
-    return hex(Date.now() / 1000) +
-      ' '.repeat(16).replace(/./g, () => hex(Math.random() * 16))
+const capitalizeFirstLetter = function(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-// Global Interface Functions
-const getAllEvents = function() {
+Client.prototype.getFullClientInfo = function() {
+    console.table(this);
+}
 
-    if (EVENT_STORAGE_COLLECTION.length > 0) {
-        let age = '(+18)';
+function Client(firstName, lastName, gender, age) {
 
-        for (let i = 0; i < EVENT_STORAGE_COLLECTION.length; i++) {
+    verifyClientCreation(firstName, lastName, gender, age);
 
-            if (EVENT_STORAGE_COLLECTION[i].areAdultsAllowed !== true) {
-                age = '(minors allowed)';
-            }
+    let client = Object.create(Client.prototype);
+
+    client.ID = generateClientID();
+    client.fullName = `${capitalizeFirstLetter(firstName)} ${capitalizeFirstLetter(lastName)}`;
+    client.gender = gender;
+    client.age = age;
     
-            console.log(`Party: ${EVENT_STORAGE_COLLECTION[i].eventName}, ${age}`);
-        }
-    } else {
-        console.log('There are no ongoing events right now :((');
-    }
-}
-
-const getAllEventsByID = function() {
-
-    if (EVENT_STORAGE_COLLECTION.length > 0) {
-
-        for (let i = 0; i < EVENT_STORAGE_COLLECTION.length; i++) {
-            console.log(`${EVENT_STORAGE_COLLECTION[i].eventName}: ${EVENT_STORAGE_COLLECTION[i].id}`);
-        }
-    } else {
-        console.log('There are no ongoing events right now :((')
-    }
-}
-
-const deleteEventById = function(eventId) {
-
-    if (!isValidString(eventId)) {
-        throw new TypeError('Please enter a valid id!');
-    }
-
-    const storageEventIndex = EVENT_STORAGE_COLLECTION
-        .findIndex(storageEventIndex => storageEventIndex.id === eventId);
-
-    if (storageEventIndex !== -1) {
-
-        EVENT_STORAGE_COLLECTION.splice(storageEventIndex, 1);
-        console.log('Event was deleted!');
-    } else {
-        console.warn('Event was not deleted! Check input please!');
-    }
-}
-
-const updateEventById = function(eventId, updatedName, updatedAdultFlag) {
-
-    const storageEventIndex = EVENT_STORAGE_COLLECTION
-        .findIndex(storageEventIndex => storageEventIndex.id === eventId);
-
-    if (!isValidString(updatedName)) {
-        throw new TypeError('Please enter a valid event name!');
-    } else {
-        EVENT_STORAGE_COLLECTION[storageEventIndex].eventName = updatedName;
-    }
-
-    if (!isValidBool(updatedAdultFlag)) {
-        throw new TypeError('Please enter a valid boolean value');
-    } else {
-        EVENT_STORAGE_COLLECTION[storageEventIndex].areAdultsAllowed = updatedAdultFlag;
-    }
-
-    getAllEvents();
-}
-
-// Ужасен начин за създаване на състояние
-let isEventCreationAllowed = true;
-let isClientCreationAllowed = true;
-
-const toggleEventCreation = function() {
-    
-    if (!isEventCreationAllowed) {
-        isEventCreationAllowed = true;
-        console.log('Event creation: enabled!');
-
-    } else {
-        isEventCreationAllowed = false;
-        console.warn('Event creation: disabled!');
-    }
-}
-
-const toggleClientCreation = function() {
-
-    if (!isClientCreationAllowed) {
-        isClientCreationAllowed = true;
-        console.log('Event creation: enabled!');
-
-    } else {
-        isClientCreationAllowed = false;
-        console.warn('Client creation: disabled!');
-    }
-}
-
-// Function constructors
-const CreateParty = function(eventName, areAdultsAllowed = true) {
-
-    if (!isEventCreationAllowed) {
-        return console.warn('Event creation is disabled!');
-    }
-
-    if (!isValidString(eventName)) {
-        throw new TypeError('Please enter a valid event name!');
-    }
-
-    if (!isValidBool(areAdultsAllowed)) {
-        throw new TypeError('Please enter a valid boolean value');
-    }
-    // Creating a closure with the allowClientsToggle()
-    // Така, мога да огранича добавянето на клиенти за текущо събитие
-    let areClientsAllowed = true;
-
-    const ADULT_AGE = 18;
-
-    const event = {
-
-        eventName,
-        areAdultsAllowed,
-        id: generateEventID(),
-        clientCollection: [],
-
-        getEventInfo: function() {
-            console.log(`Name: ${this.eventName}, age restriction: ${!this.areAdultsAllowed}, ID: ${this.id}`);
-        },
-
-        addClient: function(obj) {
-
-            if (!areClientsAllowed) {
-                return console.log('You cannot add clients to this event yet!');
-            }
-
-            if (areAdultsAllowed && obj.age < ADULT_AGE) {
-                throw new Error('Sorry, you are too young for this party! Come back next year :)');
-
-            } else if (this.clientCollection.find(client => client.id === obj.id)) {
-                throw new Error('You cannot add same client to the event!');
-
-            } else {
-                this.clientCollection.push(obj);
-
-                console.log(`Client successfully added to this event.`);
-            }
-        },
-        getClientsInfo: function() {
-
-            if (this.clientCollection.length > 0) {
-
-                console.log(this.clientCollection
-                    .map(client => `${client.fullName}, age: ${client.age}, ID: ${client.id}`));
-            } else {
-                console.log('There are no clients for this event yet');
-            }
-        },
-        filterClientsByGender: function(sex) {
-
-            if (this.clientCollection.length > 0) {
-                // Тук функцията връща обекти в масив
-                console.log(this.clientCollection
-                    .filter(client => (client.sex === sex)));
-            } else {
-                console.log('There are no clients for this event yet');
-            }
-        },
-        deleteClient: function(id) {
-
-            if (this.clientCollection.length > 0) {
-
-                this.clientCollection = this.clientCollection.filter((item) => {
-                    return item.id != id;
-                });
-
-                console.log(`Client deleted successfully.`);
-            } else {
-                console.log('There are no clients for this event yet');
-            }
-        },
-        allowClientsToggle: function() {
-            areClientsAllowed = false;
-        }
-    }
-
-    EVENT_STORAGE_COLLECTION.push(event);
-
-    return event;
-}
-
-const CreateClient = function(firstName, lastName, sex, age) {
-
-    if (!isClientCreationAllowed) {
-        return console.warn('Client creation is disabled!');
-    }
-
-    if (!isNameValid(firstName)) {
-        throw new TypeError('Please enter a valid first name!');
-    }
-
-    if (!isNameValid(lastName)) {
-        throw new TypeError('Please enter a valid last name!');
-    }
-
-    if (!isSexValid(sex)) {
-        throw new TypeError('Please enter a valid gender!');
-    }
-
-    if (!isAgeValidInt(age)) {
-        throw new TypeError('Please enter a valid age!');
-    }
-
-    const client = {
-        fullName: `${capitalizeFirstLetter(firstName)} ${capitalizeFirstLetter(lastName)}`,
-        sex: `${sex}`,
-        age: age,
-        id: generateClientID(),
-        getClientInfo: function() {
-            console.log(`Client: ${this.fullName}, Age: ${this.age}, Gender: ${this.sex}`);
-        },
-        getClientID: function() {
-            console.log(`Client: ${this.fullName}, ID: ${this.id}`);
-        }
-    }
     return client;
+
 }
 
-// #### Boilerplate testing objects
+const escapeParty = Party('Hells Angels New Year Party');
+escapeParty.getFullEventInfo();
 
-// const deluxParty  = CreateParty('Asenovgrad Delux 2020');
-// const escapeParty = CreateParty('Plovdiv Escape 2021', false);
+const konstantin = Client('konstantin', 'gogov', 'male', 18);
+konstantin.getFullClientInfo();
 
-// const ivan        = CreateClient('Ivan', 'Dimitrov', 'male', 26);
-// const vanesa      = CreateClient('Vanesa', 'Ivanova', 'female', 26);
-// const mihaela     = CreateClient('Mihaela', 'Atanasova', 'female', 22);
-// const qnica       = CreateClient('Qnica', 'Zelenkova', 'female', 19);
-// const doktora     = CreateClient('Profesor', 'Mutavchiivski', 'male', 17);
+const kremena = Client('Kremena', 'Ivanova', 'female', 19);
 
+escapeParty.addClient(konstantin);
+escapeParty.addClient(kremena);
 
-// deluxParty.addClient(ivan);
-// deluxParty.addClient(vanesa);
-// deluxParty.addClient(mihaela);
-
-// deluxParty.getEventInfo();
-// deluxParty.filterClientsByGender('female');
-// deluxParty.getClientsInfo();
+listAllParties();
