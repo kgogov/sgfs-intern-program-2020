3ac25a03e7fc32d2c6232cbf41c783eade7afd43
Konstantin Gogov <konstantin.gogov@gmail.com>
2020-11-30 13:51:45 +0200

    I've messed up with these commits. Need to rebase

---
+++ week4/iteration1/js/clock.js
@@ -0,0 +1,25 @@
+const secondHand    = document.querySelector('.second-hand');
+const minHand       = document.querySelector('.min-hand');
+const hourHand      = document.querySelector('.hour-hand');
+const rotateDegrees = 90;
+
+const setDate = () =>  {
+    const now                   = new Date();
+
+    const seconds               = now.getSeconds();
+    const secondsDegrees        = ((seconds / 60) * 360) + rotateDegrees;
+    secondHand.style.transform  = `rotate(${secondsDegrees}deg)`;
+
+    const mins                  = now.getMinutes();
+    const minsDegrees           = ((mins / 60) * 360) + rotateDegrees;
+    minHand.style.transform    = `rotate(${minsDegrees}deg)`;
+
+    const hour                  = now.getHours();
+    const hourDegrees           = ((hour / 12) * 360) + rotateDegrees;
+    hourHand.style.transform    = `rotate(${hourDegrees}deg)`;
+
+
+    [secondHand, minHand, hourHand].forEach(el => el.style.transitionDuration = (seconds === 0) ? '0s' : '0.05s');
+}
+
+setInterval(setDate, 1000);
